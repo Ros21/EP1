abc601fab440bdb35522fc2fdde12add8e957f1e
Roshan <11rshan@gmail.com>
2017-01-31 11:15:22 +0545

    Discussion Forum completed

--- app/src/main/java/com/example/android/engineeringpaathshala/QAActivity.java
+++ app/src/main/java/com/example/android/engineeringpaathshala/QAActivity.java
@@ -5,8 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,6 +27,22 @@ public class QAActivity extends AppCompatActivity {
         s = getIntent().getStringExtra("Question");
         textViewQ = (TextView) findViewById(R.id.textViewMainQuestion);
         textViewQ.setText(s);
+
+        ListView listView =(ListView) findViewById(R.id.listViewAnswers);
+        DatabaseReference ref1 = FirebaseDatabase.getInstance().getReference("DiscussionAns");
+        DatabaseReference ref = ref1.child(s);
+        final FirebaseListAdapter<String> adapter =
+                new FirebaseListAdapter<String>(QAActivity.this, String.class, android.R.layout.simple_list_item_1, ref) {
+                    @Override
+                    protected void populateView(View v, String model, int position) {
+
+                        TextView text = (TextView) v.findViewById(android.R.id.text1);
+                        text.setText(model);
+
+                    }
+                };
+        listView.setAdapter(adapter);
+
 
     }
 
